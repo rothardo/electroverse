9ceed67aa46e77e69aa57308e8129d4b9bd9c462
Ashwin Temkar <ashwintemkar@gmail.com>
2024-10-02 07:15:32 +0530

    added reusable button

---
+++ components/ui/Button.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+interface IProps {
+  text: string;
+  type?: "button" | "reset" | "submit";
+  disabled?: boolean;
+  onClick?: () => void;
+}
+
+const Button = ({
+  text,
+  onClick,
+  type = "button",
+  disabled = false,
+}: IProps) => {
+  return (
+    <button
+      disabled={disabled}
+      type={type}
+      className={`h-8 px-4 py-2 rounded-md border border-gray-300 text-gray-800 text-lg bg-white transition-colors duration-300 select-none ${
+        disabled
+          ? "cursor-default bg-gray-300"
+          : "hover:bg-gray-200 active:bg-gray-300"
+      }`}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
